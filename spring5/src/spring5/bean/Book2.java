f2a12f91e7d5bcedd6212422b40746d484152414
CYZ <1822550890@qq.com>
2021-04-07 17:49:00 +0100

    基于xml方式的bean管理中的使用命名空间进行属性的注入

---
+++ spring5/src/spring5/bean/Book2.java
@@ -0,0 +1,8 @@
+package spring5.bean;
+
+/**
+ * @author Chenyang
+ * @create 2021-04-07-17:40
+ */
+public class Book2 extends Book{
+}
--- spring5/src/spring5/test/TestSpring5.java
+++ spring5/src/spring5/test/TestSpring5.java
@@ -4,6 +4,7 @@ import org.junit.Test;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 import spring5.bean.Book;
+import spring5.bean.Book2;
 import spring5.bean.User;
 import spring5.bean.Order;
 
@@ -12,11 +13,19 @@ import spring5.bean.Order;
  * @create 2021-04-06-14:48
  */
 public class TestSpring5 {
+    @Test
+    public void testBook2() {
+        ApplicationContext context = new ClassPathXmlApplicationContext("bean1.xml");
+        Book2 book2 = context.getBean("book2", Book2.class);
+        System.out.println(book2);
+        book2.testBook();
+    }
 
     @Test
     public void testOrder() {
         ApplicationContext context = new ClassPathXmlApplicationContext("bean1.xml");
         Order order = context.getBean("order", Order.class);
+        System.out.println(order);
         order.testOrder();
     }
 
@@ -24,6 +33,7 @@ public class TestSpring5 {
     public void testBook() {
         ApplicationContext context = new ClassPathXmlApplicationContext("bean1.xml");
         Book book = context.getBean("book", Book.class);
+        System.out.println(book);
         book.testBook();
     }
 
