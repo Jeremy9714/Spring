e5f52d433598c423f68ba99c5637c682461312b4
CYZ <1822550890@qq.com>
2021-04-07 16:55:05 +0100

    first commit about spring

---
+++ spring5/src/spring5/User.java
@@ -0,0 +1,12 @@
+package spring5;
+
+/**
+ * @author Chenyang
+ * @create 2021-04-06-14:43
+ */
+public class User {
+
+    public void add(){
+        System.out.println("add......");
+    }
+}
---
+++ spring5/src/spring5/test/TestSpring5.java
@@ -0,0 +1,27 @@
+package spring5.test;
+
+import org.junit.Test;
+import org.springframework.beans.factory.BeanFactory;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+import spring5.User;
+
+/**
+ * @author Chenyang
+ * @create 2021-04-06-14:48
+ */
+public class TestSpring5 {
+
+    @Test
+    public void testAdd(){
+        //加载spring配置文件
+        ApplicationContext context = new ClassPathXmlApplicationContext("bean1.xml");
+//        BeanFactory context = new ClassPathXmlApplicationContext("bean1.xml");
+
+        //获取配置创建的对象
+        User user = context.getBean("user", User.class);
+
+        System.out.println(user);
+        user.add();
+    }
+}
