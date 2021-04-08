86212f27f7e8fec1816df28745144ed6a579184d
CYZ <1822550890@qq.com>
2021-04-08 17:32:50 +0100

    外部属性文件的引入到配置文件中

--- spring5/day_02/src/test/TestCollection.java
+++ spring5/day_02/src/test/TestCollection.java
@@ -5,9 +5,14 @@ import bean.Orders;
 import collection.Book;
 import collection.Course;
 import collection.Student;
+import com.alibaba.druid.pool.DruidDataSource;
+import com.alibaba.druid.pool.DruidPooledConnection;
 import org.junit.Test;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+import java.sql.Connection;
+import java.sql.SQLException;
 
 /**
  * @author Chenyang
