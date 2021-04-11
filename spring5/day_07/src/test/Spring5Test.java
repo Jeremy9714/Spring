fd0e5c3c37221d8c2d2b31b0dfea72f358df0bfb
CYZ <1822550890@qq.com>
2021-04-11 15:59:19 +0100

    Log4j2通用的日志封装及输出日志的两种方式

---
+++ spring5/day_07/src/test/Spring5Test.java
@@ -0,0 +1,15 @@
+package test;
+
+import org.junit.Test;
+
+/**
+ * @author Chenyang
+ * @create 2021-04-11-15:34
+ */
+public class Spring5Test {
+
+    @Test
+    public void test1() {
+
+    }
+}
---
+++ spring5/day_07/src/test/UserLog.java
@@ -0,0 +1,19 @@
+package test;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+/**
+ * 手动输出日志
+ * @author Chenyang
+ * @create 2021-04-11-15:39
+ */
+public class UserLog {
+
+    private static final Logger log = LoggerFactory.getLogger(UserLog.class);
+
+    public static void main(String[] args) {
+        log.info("hello log4j2");
+        log.warn("hello log4j2");
+    }
+}
